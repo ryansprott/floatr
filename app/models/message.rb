13626efe346974cb7d728643f0c57296d05de435
Ryan Sprott <rdsprott@gmail.com>
2021-02-05 13:14:19 -0800

    Add dependent: :destroy

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  has_one :course
-  has_one :dimension
-  has_one :mystery
-  has_one :position
+  has_one :course, dependent: :destroy
+  has_one :dimension, dependent: :destroy
+  has_one :mystery, dependent: :destroy
+  has_one :position, dependent: :destroy
 end
