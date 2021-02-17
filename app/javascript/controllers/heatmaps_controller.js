bf920d52691b41d05928eddf5707d607926d2003
Ryan Sprott <rdsprott@gmail.com>
2021-02-16 20:16:00 -0800

    Show heatmaps and paths on the same map.

--- app/javascript/controllers/heatmaps_controller.js
+++ app/javascript/controllers/heatmaps_controller.js
@@ -10,25 +10,128 @@ export default class extends Controller {
   }
 
   async initMap() {
-    let resp = await fetch(`/sources/${this.mapTarget.dataset.src}/heatmaps.json`)
-    let data = await resp.json()
-    let heatmapData = data.map((el) => {
-      return {
-        weight: el.weight,
-        location: new google.maps.LatLng(el.lat, el.lon),
-      }
-    })
+    let resp1 = await fetch(`/sources/${this.mapTarget.dataset.src}/heatmaps.json`)
+    let resp2 = await fetch(`/sources/${this.mapTarget.dataset.src}/positions.json`)
+    let heatmapData = await resp1.json()
+    let positionData = await resp2.json()
 
-    this.map = new google.maps.Map(this.mapTarget, {
-      center: heatmapData[parseInt(heatmapData.length / 2)].location,
-      zoom: 13,
+    let poly = new google.maps.Polyline({
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 1.5,
+    });
+
+    let path = poly.getPath();
+    let bounds = new google.maps.LatLngBounds();
+
+    positionData.forEach((el) => {
+      let pos = new google.maps.LatLng(el.lat, el.lon)
+      path.push(pos)
+      bounds.extend(pos)
     })
 
     let heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapData,
-      opacity: 1.0
+      data: heatmapData.map((el) => {
+        return {
+          weight: el.weight,
+          location: new google.maps.LatLng(el.lat, el.lon),
+        }
+      }),
+      opacity: 1.0,
+      radius: 12,
     })
 
+    let last = positionData.pop();
+    let marker = new google.maps.Marker({
+      position: new google.maps.LatLng(last.lat, last.lon),
+    });
+
+    this.map = new google.maps.Map(this.mapTarget, {
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
+    });
+    this.map.fitBounds(bounds);
+
     heatmap.setMap(this.map);
+    poly.setMap(this.map);
+    marker.setMap(this.map);
   }
 }
