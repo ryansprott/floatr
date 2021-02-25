ba7a25f0b7d255799d3f54f126b6c48f30e5553d
Ryan Sprott <rdsprott@gmail.com>
2021-02-24 18:02:06 -0800

    Add instance variables.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def show
-    source = Source.find(params[:source_id])
+    @source = Source.find(params[:source_id])
 
-    messages = source.messages
+    @messages = @source.messages
       .where(type: params[:id].to_i)
       .includes(
         "type_#{params[:id]}_specific".to_sym,
@@ -10,14 +10,14 @@ class MessagesController < ApplicationController
         :dimension,
       )
 
-    @message_headers = messages
+    @message_headers = @messages
       .first
       .specific_attributes
       .keys
       .map(&:titleize)
       .map(&:humanize)
 
-    @message_rows = messages
+    @message_rows = @messages
       .map(&:specific_attributes)
       .map(&:values)
       .uniq
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,8 +1,8 @@
 class StaticsController < ApplicationController
   def show
-    source = Source.find(params[:source_id])
+    @source = Source.find(params[:source_id])
 
-    messages = source
+    @messages = @source
       .messages
       .where(type: params[:id].to_i)
       .includes(
@@ -10,26 +10,26 @@ class StaticsController < ApplicationController
         :dimension,
       )
 
-    @message_headers = messages
+    @message_headers = @messages
       .first
       .specific_attributes
       .keys
       .map(&:titleize)
       .map(&:humanize)
 
-    @message_rows = messages
+    @message_rows = @messages
       .map(&:specific_attributes)
       .map(&:values)
       .uniq
 
-    @dimension_headers = messages
+    @dimension_headers = @messages
       .first
       .dimension_attributes
       .keys
       .map(&:titleize)
       .map(&:humanize)
 
-    @dimension_rows = messages
+    @dimension_rows = @messages
       .map(&:dimension_attributes)
       .map(&:values)
       .uniq
