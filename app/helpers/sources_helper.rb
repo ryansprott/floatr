704cbe31ba6bcb577df8cdb47fc76542bd1ad665
Ryan Sprott <rdsprott@gmail.com>
2021-02-26 21:35:10 -0800

    Extract partials for message links.

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -24,39 +24,78 @@ module SourcesHelper
   def parse_message_type(message_type, total_messages)
     case message_type
     when 1
-      "Class A position report type 1 (#{total_messages})"
+      render "shared/message_info", {
+        message_description: "Class A position report type 1",
+        total_messages: total_messages,
+      }
     when 2
-      "Class A position report type 2 (#{total_messages})"
+      render "shared/message_info", {
+        message_description: "Class A position report type 2",
+        total_messages: total_messages,
+      }
     when 3
-      "Class A position report type 3 (#{total_messages})"
+      render "shared/message_info", {
+        message_description: "Class A position report type 3",
+        total_messages: total_messages,
+      }
     when 4
-      "Base station report"
+      render "shared/message_info", {
+        message_description: "Base station report",
+        total_messages: total_messages,
+      }
     when 5
-      link_to "Class A static data report (#{total_messages})",
-        source_static_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Class A static data report",
+        total_messages: total_messages,
+        link_path: source_static_path(@source, message_type),
+      }
     when 8
-      link_to "Binary broadcast message (#{total_messages})",
-        source_message_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Binary broadcast message",
+        total_messages: total_messages,
+        link_path: source_message_path(@source, message_type),
+      }
     when 9
-      link_to "SAR aircraft position report (#{total_messages})",
-        source_message_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "SAR aircraft position report",
+        total_messages: total_messages,
+        link_path: source_message_path(@source, message_type),
+      }
     when 10
-      link_to "UTC/date inquiry (#{total_messages})",
-        source_message_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "UTC/date inquiry",
+        total_messages: total_messages,
+        link_path: source_message_path(@source, message_type),
+      }
     when 15
-      link_to "Interrogation (#{total_messages})",
-        source_message_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Interrogation",
+        total_messages: total_messages,
+        link_path: source_message_path(@source, message_type),
+      }
     when 18
-      "Class B position report (#{total_messages})"
+      render "shared/message_info", {
+        message_description: "Class B position report",
+        total_messages: total_messages,
+      }
     when 21
-      link_to "Aid-to-navigation report (#{total_messages})",
-        source_static_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Aid-to-navigation report",
+        total_messages: total_messages,
+        link_path: source_static_path(@source, message_type),
+      }
     when 24
-      link_to "Static data report (#{total_messages})",
-        source_static_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Static data report",
+        total_messages: total_messages,
+        link_path: source_static_path(@source, message_type),
+      }
     when 27
-      link_to "Long range AIS broadcast message (#{total_messages})",
-        source_message_path(@source, message_type)
+      render "shared/message_link", {
+        message_description: "Long range AIS broadcast message",
+        total_messages: total_messages,
+        link_path: source_message_path(@source, message_type),
+      }
     else
       message_type
     end
