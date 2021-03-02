410fbc9ec703dffd071ddc5b44414c1757da2ab4
Ryan Sprott <rdsprott@gmail.com>
2021-03-02 12:44:33 -0800

    Remove aliased attributes.

--- app/models/type8_specific.rb
+++ app/models/type8_specific.rb
@@ -1,7 +1,3 @@
 class Type8Specific < Specific
   belongs_to :message, optional: true
-
-  alias_attribute :beam, :dimension_beam
-  alias_attribute :draught, :dimension_draught
-  alias_attribute :length, :dimension_length
 end
