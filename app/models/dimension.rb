424ad3c7998eadedc7df7f750750fb2e4bb6aefa
Ryan Sprott <rdsprott@gmail.com>
2021-12-23 10:07:14 -0800

    WIP for dimensions.

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -8,15 +8,27 @@ class Dimension < ActiveRecord::Base
 
   include SerializesAttributes
 
+  def weird?
+    ship_dimension_to_port == 2 &&
+    ship_dimension_to_starboard == 0 &&
+    ship_dimension_to_bow == 16 &&
+    ship_dimension_to_stern == 130
+  end
+
   def totally_invalid?
     width_invalid? && length_invalid?
   end
 
   def total_area
-    return 1 if totally_invalid?
+    return 1 if totally_invalid? || weird?
     return width_in_meters if length_invalid?
     return length_in_meters if width_invalid?
     (length_in_meters * width_in_meters).to_i
+  end
+
+  def aspect_ratio
+    return 1 if weird?
+    return length_in_meters.to_f / width_in_meters.to_f
   end
 
   private
