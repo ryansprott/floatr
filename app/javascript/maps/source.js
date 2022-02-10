7b73ace688461cb7b69e6a154104266993f0a0af
Ryan Sprott <rdsprott@gmail.com>
2022-02-09 20:53:01 -0800

    Refactor JS source model.

--- app/javascript/maps/source.js
+++ app/javascript/maps/source.js
@@ -8,9 +8,13 @@ export default class Source {
   }
 
   getFilteredPositions() {
-    const output = this.positions.filter((position) => {
-      return (position && position.lat && position.lon) ? position.distance > 0.0 : false
-    })
+    const output = this.positions.filter(
+      (position) => {
+        return (position && position.lat && position.lon)
+          ? position.distance > 0.0
+          : false
+      }
+    )
     return output.length > 1 ? output : []
   }
 
@@ -20,17 +24,28 @@ export default class Source {
   }
 
   getMaxDistance() {
-    return Math.max(...this.getFilteredPositions().map(position => position.distance))
+    return Math.max(
+      ...this.getFilteredPositions().map(
+        position => position.distance
+      )
+    )
   }
 
   getAverageSpeed() {
-    const sum = this.getFilteredPositions().reduce((acc, position) => acc + parseFloat(position.speed), 0)
-    const avg = sum / this.getFilteredPositions().length
-    return Math.round(avg * 100) / 100 || "STOP"
+    const sum = this.getFilteredPositions().reduce(
+      (acc, position) => acc + parseFloat(position.speed),
+      0
+    )
+    const averageSpeed = sum / this.getFilteredPositions().length
+    return averageSpeed > 1.0
+      ? Math.round(averageSpeed * 100) / 100
+      : "STOP"
   }
 
   getLastCourse() {
-    const pos = this.getFilteredPositions().filter(el => el.course !== null).pop()
+    const pos = this.getFilteredPositions().filter(
+      el => el.course !== null
+    ).pop()
     return pos ? pos.course : "NONE"
   }
 }
